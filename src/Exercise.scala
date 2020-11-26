28fe968facd1230f229a6ad9e269d069fe448b63
itoktsnhc <itoktsnhc@hotmail.com>
2020-11-26 10:37:33 +0800

    exercise 2.2

--- src/Exercise.scala
+++ src/Exercise.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 object Exercise {
   //exercise 2.1
   def fibonacci(n: Int): Int = {
@@ -7,6 +9,20 @@ object Exercise {
     } else fibonacci(n - 1) + fibonacci(n - 2)
   }
 
+  //exercise 2.2
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    @tailrec
+    def loop(n: Int): Boolean = {
+      if (n >= as.length) true
+      else if (!ordered(as(n - 1), as(n))) false
+      else loop(n + 1)
+    }
+
+    loop(1)
+  }
+
   def main(args: Array[String]): Unit =
-    println(fibonacci(10))
+    println(isSorted(Array(1, 2, 2, 4, 5), (i: Int, j: Int) => {
+      i <= j
+    }))
 }
--- src/MyModule.scala
+++ src/MyModule.scala
@@ -24,15 +24,28 @@ object MyModule {
     msg.format(n, factorial(n))
   }
 
-  def formatResult(name: String, n: Int, f: Int => Int) = {
+  def formatResult(name: String, n: Int, f: Int => Int): String = {
     val msg = "The %s of %d is %d"
     msg.format(name, n, f(n))
   }
 
+  def findFirst[A](as: Array[A], p: A => Boolean): Int = {
+    @tailrec
+    def loop(n: Int): Int = {
+      if (n >= as.length) -1
+      else if (p(as(n))) n
+      else loop(n + 1)
+    }
 
-  def main(args: Array[String]) = {
-    println(formatResult("absolute val", -42, abs))
-    println(formatResult("factorial", 10, factorial))
+    loop(0)
+  }
+
+
+  def main(args: Array[String]): Unit = {
+//    println(formatResult("absolute val", -42, abs))
+//    println(formatResult("factorial", 10, factorial))
+    val array = Array("hello","world","scala");
+    println(findFirst(array, (s: String) => s == "scala"))
 
   }
 
